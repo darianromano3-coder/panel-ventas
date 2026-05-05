54b1209eaab20097fd04fbdb4f2f60bfbe584247
darianromano3-coder <darianromano3@gmail.com>
2026-05-05 09:44:01 -0300

    Delete app.js

--- app.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_URL = "https://agwmihvupgtersrmotfq.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnd21paHZ1cGd0ZXJzcm1vdGZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0MjYxMDgsImV4cCI6MjA5MzAwMjEwOH0.FgqFNiUBwZcMUrFCOJLrDsY8gaF5YCVjROsuFm47Oos";
-
-const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
